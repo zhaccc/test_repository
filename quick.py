efba143faa5dba13f4081a388c9d8cd40713b68d
Zeljko <zfurjanx@gmail.com>
2017-02-15 18:34:37 +0100

    Adding commit message in text editor.
    So I am trying out adding a commit message from VIM text editor
    and it is going fine for now but we will se how this works when we
    use "git log" command.

--- quick.py
+++ quick.py
@@ -3,7 +3,7 @@ def quick_add(*args):
 
 print quick_add(1,2,3,4,5,6,7)
 print quick_add(2,4,6,8,10)
-print quick_add(2,2,2,2,222222)
+print quick_add(2,2,2,2,222223)
 print quick_add(4444,33)
 print quick_add(3244,32312)
 print quick_add(9862,122,333)
